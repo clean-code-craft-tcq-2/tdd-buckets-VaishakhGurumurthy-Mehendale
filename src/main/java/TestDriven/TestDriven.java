bbe87f3df3a7cea69e813d09f0235b7d7aa8795f
Vaishakh G M <mehendalevgm@gmail.com>
2022-03-19 12:53:48 +0530

    Second Commit

--- src/main/java/TestDriven/TestDriven.java
+++ src/main/java/TestDriven/TestDriven.java
@@ -1,5 +1,8 @@
 package TestDriven;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,72 @@ public class TestDriven
 {
     public static String getRange( int[] values )
     {
-        return "4-5, 2";
+        int[] sortedValues = Arrays.stream(values).sorted().toArray();
+        ArrayList<ArrayList<Integer>> rangeGroups = splitIntoRanges(sortedValues);
+        rangeGroups = removeDuplicates(rangeGroups);
+        HashMap rangeCounts = getRangeCounts(rangeGroups);
+        String masterOutout = convertToString(rangeCounts);
+
+        return masterOutout;
+    }
+
+    public static ArrayList<ArrayList<Integer>> splitIntoRanges(int[] values){
+
+        ArrayList<Integer> range = new ArrayList<>();
+        ArrayList<Integer> sourceInput = (ArrayList<Integer>) Arrays.stream(values).boxed().collect(Collectors.toList());
+        sourceInput.add(-999);
+        ArrayList<ArrayList<Integer>> rangeSet = new ArrayList<>();
+
+        range.add(sourceInput.get(0));
+        for( int i=1; i<sourceInput.size(); i++){
+
+            if( sourceInput.get(i) - 1 <= sourceInput.get(i-1) && i!= sourceInput.size()-1 ){
+                range.add(sourceInput.get(i));
+            }else {
+                rangeSet.addAll(Collections.singleton((ArrayList<Integer>) range.stream().map(val -> new Integer(val)).collect(Collectors.toList())));
+                range.clear();
+                range.add(sourceInput.get(i));
+            }
+
+        }
+
+        return rangeSet;
+
+    }
+
+
+    public static ArrayList<ArrayList<Integer>> removeDuplicates(ArrayList<ArrayList<Integer>> rangeGroups){
+
+        ArrayList<ArrayList<Integer>> newRangeGroup = new ArrayList<>();
+        for(ArrayList<Integer> range : rangeGroups){
+            if( range.get(0).intValue() != range.get(range.size()-1).intValue() )
+                newRangeGroup.addAll(Collections.singleton((ArrayList<Integer>) range.stream().map(val -> new Integer(val)).collect(Collectors.toList())));
+        }
+        return newRangeGroup;
+
+    }
+
+    public static HashMap<String, String> getRangeCounts(ArrayList<ArrayList<Integer>> rangeGroups){
+
+        HashMap<String, String> rangeCounts = new HashMap<>();
+
+        for(ArrayList<Integer> range : rangeGroups){
+            String rangeString = range.get(0) + "-" + range.get(range.size()-1);
+            String rangeLength = String.valueOf(range.size());
+            rangeCounts.put(rangeString, rangeLength);
+        }
+
+        return rangeCounts;
+    }
+
+    public static String convertToString(HashMap<String, String> rangeCounts){
+        String masterOutput = new String();
+
+        Enumeration<String> keys = Collections.enumeration(rangeCounts.keySet());
+        while (keys.hasMoreElements()){
+            String rangeAsKey = keys.nextElement();
+            masterOutput = masterOutput + rangeAsKey + ", " + rangeCounts.get(rangeAsKey) + "\n";
+        }
+        return masterOutput.trim();
     }
 }
--- src/test/java/TestDriven/TestDrivenTest.java
+++ src/test/java/TestDriven/TestDrivenTest.java
@@ -12,10 +12,37 @@ public class TestDrivenTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void testRangeValues()
+    public void testRangeValues(int[] inputValues, String expectedOutput)
     {
-        int[] inputValues = new int[]{4, 5};
-        assertTrue( TestDriven.getRange(inputValues) == "4-5, 2" );
+        //System.out.println(expectedOutput);
+        assertTrue(TestDriven.getRange(inputValues).equals(expectedOutput));
+    }
+
+    @Test
+    public void passInputsToTest(){
+        int[] inputValues1 = new int[]{1,3, 3, 1, 5, 4, 10, 11, 12, 12, 4, 17};
+        String expectedOutput1 = "10-12, 4\n" + "3-5, 5";
+        testRangeValues(inputValues1, expectedOutput1);
+
+        int[] inputValues2 = new int[]{1,3, 3, 1, 5, 4, 10, 11, 12, 12, 4, 17, 18};
+        String expectedOutput2 = "17-18, 2\n" + "10-12, 4\n" + "3-5, 5";
+        testRangeValues(inputValues2, expectedOutput2);
+
+        int[] inputValues3 = new int[]{1};
+        String expectedOutput3 = "";
+        testRangeValues(inputValues3, expectedOutput3);
+
+        int[] inputValues4 = new int[]{1,1,1,1,1,1};
+        String expectedOutput4 = "";
+        testRangeValues(inputValues4, expectedOutput4);
+
+        int[] inputValues5 = new int[]{1, 2, 3, 4, 5, 6, 8, 200, 201, 222, 14, 15, 19};
+        String expectedOutput5 = "200-201, 2\n" + "14-15, 2\n" + "1-6, 6";
+
+        testRangeValues(inputValues5, expectedOutput5);
+
+        int[] inputValues6 = new int[]{};
+        String expectedOutput6 = "";
+        testRangeValues(inputValues6, expectedOutput6);
     }
 }
